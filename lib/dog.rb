d5aebe3675390ca8131f0fa7f94fe4b44c5543ac
Shay <shay2442@yahoo.com>
2022-04-15 14:16:46 -0500

    commit tree b42dff9b18f34735cac85fc1cc04d44794d44471

---
+++ lib/dog.rb
@@ -0,0 +1,17 @@
+class Dog
+    def bark
+      puts "Woof!"
+    end
+
+    def sit
+        puts "The Dog is sitting"
+    end
+  end
+  
+  fido = Dog.new
+  snoopy = Dog.new
+
+  fido.bark
+  snoopy.bark
+  fido.sit
+  snoopy.sit
---
+++ lib/person.rb
@@ -0,0 +1,15 @@
+class Person 
+    def walk
+        puts "The Person is walking"
+    end
+
+    def talk
+        puts "Hello World!"
+    end
+end
+
+person = Person.new
+person.walk
+
+shay = Person.new
+shay.talk
